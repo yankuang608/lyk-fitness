63c640e20322c588e6b3df50944b596567ba552f
kuangyan <kuangyan@didiglobal.com>
2020-10-01 10:12:34 +0800

    picker

--- plan/src/App.vue
+++ plan/src/App.vue
@@ -15,30 +15,94 @@
 </template>
 
 <script>
-import mockData from './mock/plan';
+import mockData from "./mock/plan";
 
 export default {
   name: "app",
   data() {
     return {
-      current: '胸',
+      current: "胸",
       list: [],
       labels: []
     };
   },
-  mounted(){
+  mounted() {
     this.initialData();
+    // const exerciseType = [{value:'常规组'},{value:'递减组'}];
+    // const repsNum = new Array(12).fill(0).map((item,i) => {return{value: i+1}});
+    // const setsNum = new Array(10).fill(0).map((item,i) => {return{value: i+1, children: repsNum}});
+    // let exerciseData = exerciseType;
+    // exerciseData.forEach(type => { type.children = setsNum; });
+    const dateSegmentData = [
+      {
+        is: "cube-date-picker",
+        title: "入学时间",
+        min: new Date(2000, 0, 1),
+        max: new Date(2030, 11, 31)
+      },
+      {
+        is: "cube-cascade-picker",
+        title: "毕业时间",
+        data: {
+          value: '组数',
+          children: {
+            value: '每组次数'
+          }
+        }
+      }
+    ];
+
+    this.dataSegmentPicker = this.$createSegmentPicker({
+      data: dateSegmentData,
+      onSelect: (selectedDates, selectedVals, selectedTexts) => {
+        this.$createDialog({
+          type: 'warn',
+          content: `Selected Items: <br/> - 入学时间: ${selectedTexts[0].join('')} <br/> - 毕业时间: ${selectedTexts[1].join('')}`,
+          icon: 'cubeic-alert'
+        }).show()
+      },
+      onNext: (i, selectedDate, selectedValue, selectedText) => {
+        dateSegmentData[1].min = selectedDate
+        if (i === 0) {
+          this.dateSegmentPicker.$updateProps({
+            data: dateSegmentData
+          })
+        }
+      }
+    });
   },
   methods: {
     selectItem(item) {
-      console.log(item.name);
+      this.dataSegmentPicker.show();
     },
-    initialData(){
+    initialData() {
       this.list = mockData.list;
       this.labels = mockData.labels;
     },
-    handleChange(){
-      
+    showDefault() {
+      this.$createActionSheet({
+        title: "容量计算器",
+        data: [
+          {
+            content: "<div>I am calculator</div>",
+            class: "calculator"
+          }
+        ],
+        onSelect: (item, index) => {
+          this.$createToast({
+            txt: `Clicked ${item.content}`,
+            type: "correct",
+            time: 1000
+          }).show();
+        },
+        onCancel: () => {
+          this.$createToast({
+            txt: "Clicked canceled",
+            type: "warn",
+            time: 1000
+          }).show();
+        }
+      }).show();
     }
   }
 };
